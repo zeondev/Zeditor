d92bf5ebfe6915014e4d1ef19ef6cca4fda2cec7
lapboi3 <74074061+ItsLap@users.noreply.github.com>
2021-05-18 20:11:39 +0100

    Delete update.py

--- update.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-def update():
-    with open("main.py", "w") as file:
-        x = requests.get('https://raw.githubusercontent.com/zeondev/Zeditor/master/main.py')
-        print(x.text)
-        file.write(x.text)
