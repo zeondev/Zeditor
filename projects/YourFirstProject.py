72ee5f05e99460882ef374f104b2fb5ccfe9d9a3
lapboi3 <74074061+ItsLap@users.noreply.github.com>
2021-05-20 22:15:35 +0100

    Added projects folder!!!

---
+++ projects/YourFirstProject.py
@@ -0,0 +1,8 @@
+print("Hello World!")
+print("This is your first project!")
+print("Have a explore and test stuff")
+print("We hope you enjoy")
+print("\n")
+print("The Zeon Team")
+print("\n")
+print("Push <CTRL-R> to run this!")
